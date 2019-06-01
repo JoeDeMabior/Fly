4811c39b0e601ac5afb2bc6c4cce1e3e752b453c
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-06-01 14:45:35 +0300

    Implement the WelcomeActivity

--- app/src/main/java/com/joe/fly/activities/WelcomeActivity.kt
+++ app/src/main/java/com/joe/fly/activities/WelcomeActivity.kt
@@ -1,13 +1,49 @@
 package com.joe.fly.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.joe.fly.R
+import com.joe.fly.services.MessageService
+import com.joe.fly.services.ServiceBuilder
+import kotlinx.android.synthetic.main.activity_welcome.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        // To be replaced by Retrofit code
+        val messageService = ServiceBuilder.buildService(MessageService::class.java)
+        val requestCall = messageService.getMessages("http://localhost:3000/messages")
+
+        requestCall.enqueue(object : Callback<String> {
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Toast.makeText(this@WelcomeActivity, "Failed to retrieve items.", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                if (response.isSuccessful) {
+                    val msg = response.body()
+                    msg?.let {
+                        message.text = msg
+                    }
+                } else {
+                    Toast.makeText(this@WelcomeActivity, "Failed to retrieve items.", Toast.LENGTH_LONG).show()
+                }
+            }
+
+        })
+    }
+
+    fun getStarted(view: View) {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
