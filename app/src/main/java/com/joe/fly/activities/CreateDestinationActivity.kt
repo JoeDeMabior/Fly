a9c2d22256dd07fcc3ba7c90a90d19cd83783396
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-06-01 12:54:14 +0300

    Design the activity for creating a new destination entry

---
+++ app/src/main/java/com/joe/fly/activities/CreateDestinationActivity.kt
@@ -0,0 +1,13 @@
+package com.joe.fly.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.joe.fly.R
+
+class CreateDestinationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_destination)
+    }
+}
